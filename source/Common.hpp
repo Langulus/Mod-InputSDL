7b1c6c5c68c5a4bb2255cd720b095eb8883efdb4
Dimo Markov <dimomarkov@gmail.com>
2024-05-30 03:20:19 +0300

    Minor improvements

--- source/Common.hpp
+++ source/Common.hpp
@@ -16,8 +16,13 @@ struct InputSDL;
 struct InputGatherer;
 struct InputListener;
 
-#define VERBOSE_INPUT(...)      Logger::Verbose(Self(), __VA_ARGS__)
-#define VERBOSE_INPUT_TAB(...)  const auto tab = Logger::Verbose(Self(), __VA_ARGS__, Logger::Tabs {})
+#if 0
+   #define VERBOSE_INPUT(...)      Logger::Verbose(Self(), __VA_ARGS__)
+   #define VERBOSE_INPUT_TAB(...)  const auto tab = Logger::Verbose(Self(), __VA_ARGS__, Logger::Tabs {})
+#else
+   #define VERBOSE_INPUT(...)      LANGULUS(NOOP)
+   #define VERBOSE_INPUT_TAB(...)  LANGULUS(NOOP)
+#endif
 
 /// Include SDL                                                               
 #include <SDL3/SDL.h>
