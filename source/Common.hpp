d67febd4a1a04be8d21fcdfebff2ff94e1a095ee
Dimo Markov <dimomarkov@gmail.com>
2024-07-10 16:46:54 +0300

    Minor improvements

--- source/Common.hpp
+++ source/Common.hpp
@@ -19,7 +19,7 @@ struct InputListener;
 #if 1
    #define VERBOSE_INPUT_ENABLED() 1
    #define VERBOSE_INPUT(...)      Logger::Input(Self(), __VA_ARGS__)
-   #define VERBOSE_INPUT_TAB(...)  const auto tab = Logger::Input(Self(), __VA_ARGS__, Logger::Tabs {})
+   #define VERBOSE_INPUT_TAB(...)  const auto tab = Logger::InputTab(Self(), __VA_ARGS__)
 #else
    #define VERBOSE_INPUT_ENABLED() 0
    #define VERBOSE_INPUT(...)      LANGULUS(NOOP)
