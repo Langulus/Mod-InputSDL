4ef424aad3f99f7d11fe608e7cd9ab32f8075075
Epixu <dimomarkov@gmail.com>
2025-02-17 14:41:09 +0200

    Now using common test library

--- test/TestInput.cpp
+++ test/TestInput.cpp
@@ -5,16 +5,9 @@
 ///                                                                           
 /// SPDX-License-Identifier: GPL-3.0-or-later                                 
 ///                                                                           
-#include "Main.hpp"
 #include <Langulus/Input.hpp>
-#include <catch2/catch.hpp>
+#include <Langulus/Testing.hpp>
 
-
-/// See https://github.com/catchorg/Catch2/blob/devel/docs/tostring.md        
-CATCH_TRANSLATE_EXCEPTION(::Langulus::Exception const& ex) {
-   const Text serialized {ex};
-   return ::std::string {Token {serialized}};
-}
 
 SCENARIO("Input handler creation", "[input]") {
    static Allocator::State memoryState;
