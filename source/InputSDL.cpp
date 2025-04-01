0566e0d7b55d652aa3fe805762d81525edcc0c4e
Epixu <dimomarkov@gmail.com>
2025-04-01 17:59:25 +0300

    Let framework include profiler if enabled

--- source/InputSDL.cpp
+++ source/InputSDL.cpp
@@ -6,7 +6,6 @@
 /// SPDX-License-Identifier: GPL-3.0-or-later                                 
 ///                                                                           
 #include "InputSDL.hpp"
-#include <Langulus/Profiler.hpp>
 
 LANGULUS_DEFINE_MODULE(
    InputSDL, 0, "InputSDL",
